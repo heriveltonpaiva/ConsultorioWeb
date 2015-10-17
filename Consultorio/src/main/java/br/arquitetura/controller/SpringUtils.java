22eef35b3ddba9ae1590209004a97cc14451cd15
Herivelton <heriveltonpaiva@gmail.com>
2015-10-16 23:03:28 -0300

    - Implementação do Logout, Exibição do nome do usuário logado e adição dos ícones no menu e na tela inicial de atendimento.

--- Consultorio/src/main/java/br/arquitetura/controller/SpringUtils.java
+++ Consultorio/src/main/java/br/arquitetura/controller/SpringUtils.java
@@ -2,13 +2,15 @@ package br.arquitetura.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SpringUtils {
 
     public static ApplicationContext ctx;
-
+    
     /**
      * Make Spring inject the application context
      * and save it on a static variable,
@@ -17,5 +19,14 @@ public class SpringUtils {
     @Autowired
     private void setApplicationContext(ApplicationContext applicationContext) {
         ctx = applicationContext;       
+        
     }
+    
+    public String getCurrentUser() {
+    	  Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    			    if (principal instanceof User) 
+    			    	return ((User) principal).getUsername();
+    			    return "";
+	}
+    
 }
